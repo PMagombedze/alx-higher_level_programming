638c7a4cf4b7b3e1ec543da832a4af1bc65fd06b
PMagombedze <percymagoras@outlook.com>
2023-06-08 23:18:04 +0200

    variable load

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == "__main__":
+    """hidden discovery"""
+    f = dir(hidden_4)
+    n = len(f)
+    for j in range(0, n):
+        if file[j][0:2] != "__":
+            print(file[j])
