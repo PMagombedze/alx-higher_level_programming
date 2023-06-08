e6bdbc33334532ec3d8b90b00dbee5892ca01191
PMagombedze <percymagoras@outlook.com>
2023-06-08 23:25:00 +0200

    easy print

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
 
 if __name__ == "__main__":
+    import hidden_4
     """hidden discovery"""
     f = dir(hidden_4)
-    n = len(f)
-    for j in range(0, n):
-        if file[j][0:2] != "__":
-            print(file[j])
+    for n in f:
+        if n[:2] != "__":
+            print(n)
