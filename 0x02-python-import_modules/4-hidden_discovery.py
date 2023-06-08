336933fc8048c9bb0b74eba2a11e20afe161f2dc
PMagombedze <percymagoras@outlook.com>
2023-06-08 23:32:56 +0200

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,2 @@
 #!/usr/bin/python3
-import hidden_4
 
-if __name__ == "__main__":
-    """hidden discovery"""
-    f = dir(hidden_4)
-    for n in f:
-        if n[:2] != "__":
-            print(n)
