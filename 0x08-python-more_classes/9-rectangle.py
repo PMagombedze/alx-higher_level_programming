f008896225bcc2767cd460dfee604d6f59e28c3b
PMagombedze <percymagoras@outlook.com>
2023-07-03 13:20:45 -0700

    update methods

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -67,7 +67,7 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """returns new Rectangle instance"""
-        return cls(size, size)
+        return (cls(size, size))
 
     def __str__(self):
         """returns printable rep of rectangle"""
