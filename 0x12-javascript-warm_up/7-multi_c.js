014c9869ce4c85983c5c6ed775dc48d37f34d427
Percy Magombedze <123000552+PMagombedze@users.noreply.github.com>
2023-09-12 04:05:20 +0200

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
-} else {
-  const num = Number(process.argv[2]);
-  for (let i = 0; i < num; i++) {
-    console.log('C is fun');
-  }
-}
