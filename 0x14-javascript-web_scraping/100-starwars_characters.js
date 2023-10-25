a276c082effdcee0e271c1f2c895e6f19e1d4417
PMagombedze <percymagoras@outlook.com>
2023-10-25 03:02:30 +0200

    semi-std

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,18 +4,18 @@ const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/';
 request.get(url + id, function (error, res, body) {
-    if (error) {
+  if (error) {
+    console.log(error);
+  }
+  const dt = JSON.parse(body);
+  const dT = dt.characters;
+  for (const i of dT) {
+    request.get(i, function (error, res, body_) {
+      if (error) {
         console.log(error);
-    }
-    const dt = JSON.parse(body);
-    const dT = dt.characters;
-    for (const i of dT) {
-        request.get(i, function (error, res, body_) {
-            if (error) {
-                console.log(error);
-            }
-            const dt_ = JSON.parse(body_);
-            console.log(dt_.name);
-        });
-    }
+      }
+      const dt_ = JSON.parse(body_);
+      console.log(dt_.name);
+    });
+  }
 });
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,19 +3,19 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
-    if (!error) {
-        const characters = JSON.parse(body).characters;
-        Chars(characters, 0);
-    }
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    Chars(characters, 0);
+  }
 });
 
-function Chars(characters, index) {
-    request(characters[index], function (error, response, body) {
-        if (!error) {
-            console.log(JSON.parse(body).name);
-            if (index + 1 < characters.length) {
-                Chars(characters, index + 1);
-            }
-        }
-    });
+function Chars (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        Chars(characters, index + 1);
+      }
+    }
+  });
 }
